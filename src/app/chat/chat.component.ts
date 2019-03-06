471efa2e754db52ec0606d40cf9b981732d4ef42
yemiwebby <yemiwebby@gmail.com>
2019-03-06 21:51:33 +0100

    modified the chat component

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Message, User, SendMessageEvent } from '@progress/kendo-angular-conversational-ui';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  public readonly user: User = {
+    id: 1
+  };
+
+  public readonly bot: User = {
+    id: 0
+  };
+
+  public messages: Message[] = [{
+    author: this.bot,
+    suggestedActions: [{
+      type: 'reply',
+      value: 'OK. Lets do this!'
+    }],
+    timestamp: new Date(),
+    text: 'Not too intelligent, but I can show you the text you entered'
+  }];
+
+  public sendMessage(e: SendMessageEvent): void {
+    const echo: Message = {
+      author: this.bot,
+      text: `You said: ${e.message.text}`
+    };
+
+    setTimeout(
+      () => this.messages = [...this.messages, e.message, echo],
+      500
+    );
   }
 
 }
